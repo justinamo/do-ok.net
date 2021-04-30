c9eafb8cb523aae63fa569a75f813ee9e72b760d
justin <justin@absurdity.home>
2021-04-30 15:49:52 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+sections = ['Home', 'Projects', 'Thoughts', 'Contact']
+
+@app.route('/')
+@app.route('/home')
+def home():
+  return render_template('home.html', navSections=sections, thisSection='Home')
+
+@app.route('/projects')
+def projects():
+  return render_template('projects.html', navSections=sections, thisSection='Projects')
+
+@app.route('/thoughts')
+def thoughts():
+  return render_template('thoughts.html', navSections=sections, thisSection='Thoughts')
+
+@app.route('/thoughts/<thoughtname>')
+def thoughtdetail(thoughtname):
+  return render_template('thoughtdetail.html', navSections=sections, thisSection='Thoughts', thoughtname=thoughtname)
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html', navSections=sections, thisSection='Contact')
