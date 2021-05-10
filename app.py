47cd0cfc799792653695e2e7e06dd2861e0b8bc6
justin <justin@absurdity.home>
2021-05-10 17:56:45 -0400

    comments and formatting

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from collections import OrderedDict
+import mysql.connector 
+
+cnx = mysql.connector.connect(user='justin', host='localhost', database='thoughts')
+cursor = cnx.cursor(buffered=True)
+
+def dispatch(query, params=None): 
+  print('executing query: ' + query)
+  cursor.execute(query, params) 
+
 app = Flask(__name__)
 
-sections = ['Home', 'Projects', 'Thoughts', 'Contact']
+sections = ['Home', 'Life', 'Projects', 'Thoughts']
 
 @app.route('/')
 @app.route('/home')
@@ -14,12 +24,40 @@ def projects():
 
 @app.route('/thoughts')
 def thoughts():
-  return render_template('thoughts.html', navSections=sections, thisSection='Thoughts')
+  posts = OrderedDict()
 
-@app.route('/thoughts/<thoughtname>')
-def thoughtdetail(thoughtname):
-  return render_template(['comments.html', 'thoughts/' + thoughtname + '-comments.html'], navSections=sections)
+  dispatch('select * from posts')
+  for (url, name, date, html) in cursor:
+    posts[url] = { 'url': url, 'name': name, 'date': date, 'html': html, 'tags': [] }
+  
+  dispatch('select * from posts_tags')
+  for (post_url, tag) in cursor:
+    print('appending tag: ', post_url, tag)
+    posts[post_url]['tags'].append(tag) 
 
-@app.route('/contact')
-def contact():
-  return render_template('contact.html', navSections=sections, thisSection='Contact')
+  return render_template('thoughts.html', navSections=sections, thisSection='Thoughts', posts=posts)
+
+@app.route('/thoughts/<thoughtname>', methods=['GET', 'POST'])
+def comments(thoughtname):
+  if request.method == 'POST':
+    print('recieved post request')
+    name = request.form['name']
+    text = request.form['comment']
+    dispatch('insert into posts_comments (post_url, name, text) values (%s, %s, %s)', (thoughtname, name, text))
+    cnx.commit()
+
+  dispatch('select * from posts where url = %s order by date desc', (thoughtname, ))
+  for (url, name, date, html) in cursor: 
+    post = { 'url': url, 'name': name, 'date': date, 'html': html, 'tags': [] }
+
+  dispatch('select * from posts_tags where post_url = %s', (thoughtname, ))
+  for (post_url, tag) in cursor:
+    print('appending tag: ', post_url, tag)
+    post['tags'].append(tag) 
+
+  comments = []
+  dispatch('select * from posts_comments where post_url = %s', (thoughtname, ))
+  for (posted_on, post_url, name, text) in cursor: 
+    comments.append({ 'posted_on': posted_on, 'post_url': post_url, 'name': name, 'text': text })
+
+  return render_template('comments.html', navSections=sections, post=post, comments=comments)
