e9123288354ef7c23909a66d0ec09c4ffcb05996
justin <justinamo@gmail.com>
2021-10-26 18:47:58 -0400

    updates

--- life_fountain.py
+++ life_fountain.py
@@ -7,9 +7,8 @@ def append_offset(drop, offset):
 def generate():
   drops = [
     { 'name': 'learning japanese', 'link': 'https://sshh.nyc/event/class-intro-to-japanese-new-12-week-session/' }
-  , { 'name': 'i/c artists webpage', 'link': '/projects#icartists' }
   , { 'name': 'personal website', 'link': '/' }
-  , { 'name': 'Do Ok EP', 'link': 'https://do-ok.bandcamp.com' }
+  , { 'name': 'do ok musica', 'link': 'https://do-ok.bandcamp.com' }
   , { 'name': 'jmail.link', 'link': 'https://jmail.link' }
   , { 'name': 'en-tranceit' }
   , ]
